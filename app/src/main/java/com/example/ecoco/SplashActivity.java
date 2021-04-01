65dba0c917ab0fc2e9898cc1759cff0633262e53
sunginkweon <sunginkweon@kakao.com>
2021-04-02 08:46:03 +0900

    .

---
+++ app/src/main/java/com/example/ecoco/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.ecoco;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 1000);
+    }
+}
